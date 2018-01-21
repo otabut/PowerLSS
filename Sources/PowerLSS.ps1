258a7aef6ff88effdd43687b9d839567d37ff8a8
otabut <33980031+otabut@users.noreply.github.com>
2018-01-21 19:38:28 +0100

    Update PowerLSS.ps1
    Increment version

--- Sources/PowerLSS.ps1
+++ Sources/PowerLSS.ps1
@@ -9,10 +9,11 @@
 
 .NOTES
     Author: Olivier TABUT
-        Version: 0.2.0 (21/01/2018)
+        0.3.0 release (28/01/2018)
     ChangeLog: 
         Initial version (14/01/2018)
         0.2.0 release (21/01/2018)
+	0.3.0 release (28/01/2018)
 
 .PARAMETER Retry
     Activate a retry if first attempt to run startup script failed
