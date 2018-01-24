198dd8c9d25ca4374e1252fbd55a827a4dc1cbaf
otabut <33980031+otabut@users.noreply.github.com>
2018-01-24 13:10:38 +0100

    Update Install-PowerLSS.tests.ps1

--- Pester/Install-PowerLSS.tests.ps1
+++ Pester/Install-PowerLSS.tests.ps1
@@ -1,188 +1,188 @@
-
-$Path = "C:\Windows\Setup\Scripts\PowerLSS"
-$LogFile = "$Path\Install-PowerLSS.log"
-
+Import-Module PowerLSS
+$Path = Split-Path((Get-Module PowerLSS).path)
+$LogFile = "$Path\Functions\Install-PowerLSS.log"
 
 Describe "Test Install-PowerLSS" {
 
   Context "'PowerLSS Scheduled Task is present/enabled'" {
 
-    It "no switches" {
-      & "$Path\Install-PowerLSS.ps1"
+    It "No switches" {
+      Install-PowerLSS -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 2
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 0
       ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 1
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 0
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 0
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 1
     }
     
-    It "only ForceCreate switch" {
-      & "$Path\Install-PowerLSS.ps1" -ForceCreate
+    It "Only Force switch" {
+      Install-PowerLSS -Force -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 2
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 1
-      ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 0
+      ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
-      ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 1
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 0
+      ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 0
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 1
     }
 
-    It "only WithoutStarting switch" {
-      & "$Path\Install-PowerLSS.ps1" -WithoutStarting
+    It "Only NoStart switch" {
+      Install-PowerLSS -NoStart -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 2
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 0
       ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 1
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 1
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 1
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 0
     }
 
-    It "ForceCreate and WithoutStarting switches" {
-      & "$Path\Install-PowerLSS.ps1" -ForceCreate -WithoutStarting
+    It "Force and NoStart switches" {
+      Install-PowerLSS -Force -NoStart -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 2
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 1
-      ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 0
+      ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
-      ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 1
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 1
+      ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 1
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 0
     }
     
     BeforeEach {
-      & "$Path\Install-PowerLSS.ps1" -WithoutStarting
-      Remove-Item $LogFile -Force
+      Install-PowerLSS -NoStart -LogFile "$LogFile"
+      Remove-Item $LogFile -Force -ErrorAction SilentlyContinue
     }
 
     AfterEach {
-      invoke-expression "SCHTASKS /DELETE /TN PowerLSS /F"
+      Remove-LSS_ScheduledTask
     }
   }
   
   Context "'PowerLSS Scheduled Task is present/disabled'" {
 
-    It "no switches" {
-      & "$Path\Install-PowerLSS.ps1"
+    It "No switches" {
+      Install-PowerLSS -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 2
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 0
       ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 0
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 0
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 1
     }
 
-    It "only ForceCreate switch" {
-      & "$Path\Install-PowerLSS.ps1" -ForceCreate
+    It "Only Force switch" {
+      Install-PowerLSS -Force -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 2
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 1
-      ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 0
+      ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
-      ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 1
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 0
+      ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 0
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 1
     }
 
-    It "only WithoutStarting switch" {
-      & "$Path\Install-PowerLSS.ps1" -WithoutStarting
+    It "Only NoStart switch" {
+      Install-PowerLSS -NoStart -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 2
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 0
       ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 1
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 1
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 0
     }
 
-    It "ForceCreate and WithoutStarting switches" {
-      & "$Path\Install-PowerLSS.ps1" -ForceCreate -WithoutStarting
+    It "Force and NoStart switches" {
+      Install-PowerLSS -Force -NoStart -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 2
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 1
-      ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 0
+      ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
-      ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 1
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 1
+      ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 1
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 0
     }
 
     BeforeEach {
-      & "$Path\Install-PowerLSS.ps1" -WithoutStarting
-      invoke-expression "SCHTASKS /CHANGE /DISABLE /TN PowerLSS"
-      Remove-Item $LogFile -Force
+      Install-PowerLSS -NoStart -LogFile "$LogFile"
+      Disable-LSS_ScheduledTask
+      Remove-Item $LogFile -Force -ErrorAction SilentlyContinue
     }
 
     AfterEach {
-      invoke-expression "SCHTASKS /DELETE /TN PowerLSS /F"
+      Remove-LSS_ScheduledTask
     }
   }
 
   Context "'PowerLSS Scheduled Task is absent'" {
 
-    It "no switches" {
-      & "$Path\Install-PowerLSS.ps1"
+    It "No switches" {
+      Install-PowerLSS -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 1
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 0
       ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 0
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 0
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 1
     }
 
-    It "only ForceCreate switch" {
-      & "$Path\Install-PowerLSS.ps1" -ForceCreate
+    It "Only Force switch" {
+      Install-PowerLSS -Force -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 1
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 0
       ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 0
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 0
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 1
     }
 
-    It "only WithoutStarting switch" {
-      & "$Path\Install-PowerLSS.ps1" -WithoutStarting
+    It "Only NoStart switch" {
+      Install-PowerLSS -NoStart -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 1
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 0
       ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 1
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 1
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 0
     }
 
-    It "ForceCreate and WithoutStarting switches" {
-      & "$Path\Install-PowerLSS.ps1" -ForceCreate -WithoutStarting
+    It "Force and NoStart switches" {
+      Install-PowerLSS -Force -NoStart -LogFile "$LogFile"
       $Trace = Get-Content $LogFile
       ($Trace -match "PowerLSS scheduled task already exists").count | should be 1
       ($Trace -match "Trying to remove PowerLSS scheduled task").count | should be 0
       ($Trace -match "PowerLSS scheduled task installed successfully").count | should be 1
       ($Trace -match "PowerLSS scheduled task was enabled successfully").count | should be 0
       ($Trace -match "PowerLSS scheduled task is ready to run").count | should be 0
-      ($Trace -match "WithoutStarting switch is present, so skipping startup").count | should be 1
+      ($Trace -match "NoStart switch is present, so skipping startup").count | should be 1
       ($Trace -match "PowerLSS scheduled task started successfully").count | should be 0
     }
 
     BeforeEach {
-      #invoke-expression "SCHTASKS /DELETE /TN PowerLSS /F"
-      Remove-Item $LogFile -Force
+      Remove-Item $LogFile -Force -ErrorAction SilentlyContinue
     }
 
     AfterEach {
-      invoke-expression "SCHTASKS /DELETE /TN PowerLSS /F"
+      Remove-LSS_ScheduledTask
     }
   }
 }
+
+Remove-Item $LogFile -Force -ErrorAction SilentlyContinue
