87bf1bc1fb34bc3b070e01471dd51fc667f267f5
otabut <33980031+otabut@users.noreply.github.com>
2018-01-26 14:34:52 +0100

    Update Set-LSS_Configuration.ps1

--- Functions/Set-LSS_Configuration.ps1
+++ Functions/Set-LSS_Configuration.ps1
@@ -1,1 +1,22 @@
+Function Set-LSS_Configuration
+{
+  Param (
+    [parameter(Mandatory=$true)][String]$ConfigurationName,
+    [parameter(Mandatory=$true)][ValidateSet("LogFile","InitialDelay","Include","Exclude","ValidExitCodes","Retry","AllowReboot","ContinueIfRebootRequest","ContinueOnFailure","DisableAtTheEnd","ConsoleOutput","Output","CustomLogging","DontRunPreActions","DontRunPostActions","ScriptsFolder","ScheduledTaskLogFile")][String]$Parameter,
+    [parameter(Mandatory=$true)][String]$Value
+  )
+  
+  If (!(Get-module PowerLSS))
+  {
+    Import-Module PowerLSS
+  }
 
+  $RegPath = "HKLM:\SOFTWARE\PowerLSS\$ConfigurationName"
+  if (!(Test-Path $RegPath -PathType Container))
+  {
+    New-Item -Path $RegPath | Out-Null
+  }
+  
+  New-ItemProperty -Path $RegPath -Name $Parameter -Value $Value -PropertyType string -Force | Out-Null
+  Write-Host "Parameter set successfully" -ForegroundColor Green
+}
