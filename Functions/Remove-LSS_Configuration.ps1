41e37ec2fe4846c1de7eb089c6b09445edb59977
otabut <33980031+otabut@users.noreply.github.com>
2018-01-26 12:45:12 +0100

    Update Remove-LSS_Configuration.ps1

--- Functions/Remove-LSS_Configuration.ps1
+++ Functions/Remove-LSS_Configuration.ps1
@@ -1,1 +1,35 @@
+Function Remove-LSS_Configuration
+{
+  Param (
+    [parameter(Mandatory=$true)][String]$ConfigurationName
+  )
+  
+  If (!(Get-module PowerLSS))
+  {
+    Import-Module PowerLSS
+  }
 
+  $RegBasePath = "HKLM:\SOFTWARE\PowerLSS"
+  $AllConfigurations = $ConfigurationName.split(',')
+
+  ForEach ($ConfigurationName in $AllConfigurations)
+  {
+    $RegPath = "$RegBasePath\$ConfigurationName"
+    if (Test-Path $RegPath -PathType Container)
+    {
+      if ($ConfigurationName -ne (Get-ItemProperty -Path $RegBasePath)."CurrentConfiguration")
+      {
+        Remove-Item -Path $RegPath -Force
+        Write-Host "Configuration $ConfigurationName has been removed successfully" -ForegroundColor Green
+      }
+      else
+      {
+        Write-Host "Configuration $ConfigurationName cannot be removed as it is the current configuration" -ForegroundColor Red
+      }
+    }
+    else
+    {
+      Write-Host "Configuration $ConfigurationName cannot be found" -ForegroundColor Yellow
+    }
+  }
+}
