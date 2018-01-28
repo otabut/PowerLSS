35722ccd5f959309f1b62465bab610e89b378079
otabut <33980031+otabut@users.noreply.github.com>
2018-01-28 13:06:04 +0100

    Update Set-LSS_Configuration.ps1

--- Functions/Set-LSS_Configuration.ps1
+++ Functions/Set-LSS_Configuration.ps1
@@ -11,12 +11,25 @@ Function Set-LSS_Configuration
     Import-Module PowerLSS
   }
 
-  $RegPath = "HKLM:\SOFTWARE\PowerLSS\$ConfigurationName"
+  $RegBasePath = "HKLM:\SOFTWARE\PowerLSS"
+  $RegPath = "$RegBasePath\$ConfigurationName"
+
+  if (!(Test-Path $RegBasePath -PathType Container))
+  {
+    New-Item -Path $RegBasePath | Out-Null
+  }
+
   if (!(Test-Path $RegPath -PathType Container))
   {
     New-Item -Path $RegPath | Out-Null
   }
   
+  if ((((Get-ChildItem -Path $RegBasePath).PSChildName).count -eq 0) -or ((Get-ItemProperty -Path $RegBasePath)."CurrentConfiguration" -eq $null))
+  {
+    New-ItemProperty -Path $RegBasePath -Name "CurrentConfiguration" -Value $ConfigurationName -PropertyType String -Force | Out-Null
+    Write-Host "Since there is no current configuration, $ConfigurationName has been set as default configuration" -ForegroundColor Yellow
+  }
+
   New-ItemProperty -Path $RegPath -Name $Parameter -Value $Value -PropertyType string -Force | Out-Null
   Write-Host "Parameter set successfully" -ForegroundColor Green
 }
