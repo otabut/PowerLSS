f9ee9e30d6321a62cd47a107ca7e5526f989df0c
otabut <33980031+otabut@users.noreply.github.com>
2018-01-28 13:15:13 +0100

    Update Set-LSS_Configuration.ps1

--- Functions/Set-LSS_Configuration.ps1
+++ Functions/Set-LSS_Configuration.ps1
@@ -6,30 +6,39 @@ Function Set-LSS_Configuration
     [parameter(Mandatory=$true)][String]$Value
   )
   
-  If (!(Get-module PowerLSS))
+  try
   {
-    Import-Module PowerLSS
-  }
+    If (!(Get-module PowerLSS))
+    {
+      Import-Module PowerLSS
+    }
 
-  $RegBasePath = "HKLM:\SOFTWARE\PowerLSS"
-  $RegPath = "$RegBasePath\$ConfigurationName"
+    $RegBasePath = "HKLM:\SOFTWARE\PowerLSS"
+    $RegPath = "$RegBasePath\$ConfigurationName"
 
-  if (!(Test-Path $RegBasePath -PathType Container))
-  {
-    New-Item -Path $RegBasePath | Out-Null
-  }
+    if (!(Test-Path $RegBasePath -PathType Container))
+    {
+      New-Item -Path $RegBasePath | Out-Null
+    }
 
-  if (!(Test-Path $RegPath -PathType Container))
-  {
-    New-Item -Path $RegPath | Out-Null
-  }
+    if (!(Test-Path $RegPath -PathType Container))
+    {
+      New-Item -Path $RegPath | Out-Null
+    }
   
-  if ((((Get-ChildItem -Path $RegBasePath).PSChildName).count -eq 0) -or ((Get-ItemProperty -Path $RegBasePath)."CurrentConfiguration" -eq $null))
-  {
-    New-ItemProperty -Path $RegBasePath -Name "CurrentConfiguration" -Value $ConfigurationName -PropertyType String -Force | Out-Null
-    Write-Host "Since there is no current configuration, $ConfigurationName has been set as default configuration" -ForegroundColor Yellow
-  }
+    if ((((Get-ChildItem -Path $RegBasePath).PSChildName).count -eq 0) -or ((Get-ItemProperty -Path $RegBasePath)."CurrentConfiguration" -eq $null))
+    {
+      New-ItemProperty -Path $RegBasePath -Name "CurrentConfiguration" -Value $ConfigurationName -PropertyType String -Force | Out-Null
+      Write-Host "Since there is no current configuration, $ConfigurationName has been set as default configuration" -ForegroundColor Yellow
+    }
 
-  New-ItemProperty -Path $RegPath -Name $Parameter -Value $Value -PropertyType string -Force | Out-Null
-  Write-Host "Parameter set successfully" -ForegroundColor Green
+    New-ItemProperty -Path $RegPath -Name $Parameter -Value $Value -PropertyType string -Force | Out-Null
+    Write-Host "Parameter set successfully" -ForegroundColor Green
+  }
+  catch
+  {
+    $ErrorMessage = $_.Exception.Message
+    $ErrorLine = $_.InvocationInfo.ScriptLineNumber
+    Write-Error -Step "Error Management" -Status "Error" -Comment "Error on line $ErrorLine. The error message was: $ErrorMessage"
+  }
 }
