013ada838e96478033cee15a2e3ba698dcb1e0de
otabut <33980031+otabut@users.noreply.github.com>
2018-01-28 13:16:03 +0100

    Update Copy-LSS_Configuration.ps1

--- Functions/Copy-LSS_Configuration.ps1
+++ Functions/Copy-LSS_Configuration.ps1
@@ -5,34 +5,43 @@ Function Copy-LSS_Configuration
     [parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][String]$Target
   )
   
-  If (!(Get-module PowerLSS))
+  try
   {
-    Import-Module PowerLSS
-  }
-
-  $ParameterList = @("LogFile","InitialDelay","Include","Exclude","ValidExitCodes","Retry","AllowReboot","ContinueIfRebootRequest","ContinueOnFailure","DisableAtTheEnd","ConsoleOutput","Output","CustomLogging","DontRunPreActions","DontRunPostActions","ScriptsFolder","ScheduledTaskLogFile")
-  $RegPathSource = "HKLM:\SOFTWARE\PowerLSS\$Source"
-  $RegPathTarget = "HKLM:\SOFTWARE\PowerLSS\$Target"
-
-  if (!(Test-Path $RegPathSource -PathType Container))
-  {
-    Write-Host "Source configuration doesn't exist" -ForegroundColor Red
-    Return
-  }
-
-  if (!(Test-Path $RegPathTarget -PathType Container))
-  {
-    New-Item -Path $RegPathTarget | Out-Null
-  }
-  
-  ForEach ($Parameter in $ParameterList)
-  {
-    $Value = (Get-ItemProperty -Path $RegPathSource)."$Parameter"
-    if ($Value)
+    If (!(Get-module PowerLSS))
     {
-      New-ItemProperty -Path $RegPathTarget -Name $Parameter -Value $Value -PropertyType string -Force | Out-Null
+      Import-Module PowerLSS
     }
-  }
 
-  Write-Host "Configuration copied successfully" -ForegroundColor Green
+    $ParameterList = @("LogFile","InitialDelay","Include","Exclude","ValidExitCodes","Retry","AllowReboot","ContinueIfRebootRequest","ContinueOnFailure","DisableAtTheEnd","ConsoleOutput","Output","CustomLogging","DontRunPreActions","DontRunPostActions","ScriptsFolder","ScheduledTaskLogFile")
+    $RegPathSource = "HKLM:\SOFTWARE\PowerLSS\$Source"
+    $RegPathTarget = "HKLM:\SOFTWARE\PowerLSS\$Target"
+
+    if (!(Test-Path $RegPathSource -PathType Container))
+    {
+      Write-Host "Source configuration doesn't exist" -ForegroundColor Red
+      Return
+    }
+
+    if (!(Test-Path $RegPathTarget -PathType Container))
+    {
+      New-Item -Path $RegPathTarget | Out-Null
+    }
+  
+    ForEach ($Parameter in $ParameterList)
+    {
+      $Value = (Get-ItemProperty -Path $RegPathSource)."$Parameter"
+      if ($Value)
+      {
+        New-ItemProperty -Path $RegPathTarget -Name $Parameter -Value $Value -PropertyType string -Force | Out-Null
+      }
+    }
+
+    Write-Host "Configuration copied successfully" -ForegroundColor Green
+  }
+  catch
+  {
+    $ErrorMessage = $_.Exception.Message
+    $ErrorLine = $_.InvocationInfo.ScriptLineNumber
+    Write-Error -Step "Error Management" -Status "Error" -Comment "Error on line $ErrorLine. The error message was: $ErrorMessage"
+  }
 }
