cb0966fdf1c1353b138df3d6f5d5a70856d111c1
otabut <33980031+otabut@users.noreply.github.com>
2018-01-28 13:16:25 +0100

    Update Remove-LSS_Configuration.ps1

--- Functions/Remove-LSS_Configuration.ps1
+++ Functions/Remove-LSS_Configuration.ps1
@@ -4,32 +4,41 @@ Function Remove-LSS_Configuration
     [parameter(Mandatory=$true)][String]$ConfigurationName
   )
   
-  If (!(Get-module PowerLSS))
+  try
   {
-    Import-Module PowerLSS
-  }
-
-  $RegBasePath = "HKLM:\SOFTWARE\PowerLSS"
-  $AllConfigurations = $ConfigurationName.split(',')
-
-  ForEach ($ConfigurationName in $AllConfigurations)
-  {
-    $RegPath = "$RegBasePath\$ConfigurationName"
-    if (Test-Path $RegPath -PathType Container)
+    If (!(Get-module PowerLSS))
     {
-      if ($ConfigurationName -ne (Get-ItemProperty -Path $RegBasePath)."CurrentConfiguration")
+      Import-Module PowerLSS
+    }
+
+    $RegBasePath = "HKLM:\SOFTWARE\PowerLSS"
+    $AllConfigurations = $ConfigurationName.split(',')
+
+    ForEach ($ConfigurationName in $AllConfigurations)
+    {
+      $RegPath = "$RegBasePath\$ConfigurationName"
+      if (Test-Path $RegPath -PathType Container)
       {
-        Remove-Item -Path $RegPath -Force
-        Write-Host "Configuration $ConfigurationName has been removed successfully" -ForegroundColor Green
+        if ($ConfigurationName -ne (Get-ItemProperty -Path $RegBasePath)."CurrentConfiguration")
+        {
+          Remove-Item -Path $RegPath -Force
+          Write-Host "Configuration $ConfigurationName has been removed successfully" -ForegroundColor Green
+        }
+        else
+        {
+          Write-Host "Configuration $ConfigurationName cannot be removed as it is the current configuration" -ForegroundColor Red
+        }
       }
       else
       {
-        Write-Host "Configuration $ConfigurationName cannot be removed as it is the current configuration" -ForegroundColor Red
+        Write-Host "Configuration $ConfigurationName cannot be found" -ForegroundColor Yellow
       }
     }
-    else
-    {
-      Write-Host "Configuration $ConfigurationName cannot be found" -ForegroundColor Yellow
-    }
+  }
+  catch
+  {
+    $ErrorMessage = $_.Exception.Message
+    $ErrorLine = $_.InvocationInfo.ScriptLineNumber
+    Write-Error -Step "Error Management" -Status "Error" -Comment "Error on line $ErrorLine. The error message was: $ErrorMessage"
   }
 }
