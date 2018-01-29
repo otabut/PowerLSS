422607485158b1137f58808494932754ca26a7fc
otabut <33980031+otabut@users.noreply.github.com>
2018-01-29 13:17:46 +0100

    Update Configure-PowerLSS.tests.ps1

--- Pester/Configure-PowerLSS.tests.ps1
+++ Pester/Configure-PowerLSS.tests.ps1
@@ -1,1 +1,78 @@
 
+Import-Module PowerLSS
+
+
+Describe "Test Configure-PowerLSS" {
+
+  Context "'SET'" {
+
+    It "Set Configuration" {
+      Set-LSS_Configuration -ConfigurationName Pester -Parameter InitialDelay -Value 99
+      (Get-LSS_Configuration -ConfigurationName Pester).InitialDelay | Should be 99
+    }
+
+    BeforeEach {
+      Function global:Write-Host() {}
+    }
+
+    AfterEach {
+      Remove-LSS_Configuration -ConfigurationName Pester
+      Remove-Item -Path Function:\Write-Host
+    }
+  }
+
+  Context "'GET'" {
+
+    It "Get 1 Configuration" {
+      (Get-LSS_Configuration -ConfigurationName Pester).InitialDelay | Should be 99
+    }
+
+    BeforeEach {
+      Function global:Write-Host() {}
+      Set-LSS_Configuration -ConfigurationName Pester -Parameter InitialDelay -Value 99
+    }
+
+    AfterEach {
+      Remove-LSS_Configuration -ConfigurationName Pester
+      Remove-Item -Path Function:\Write-Host
+    }
+  }
+
+  Context "'COPY'" {
+
+    It "Copy Configuration" {
+      Copy-LSS_Configuration -Source Pester -Target Pester2
+      (Get-LSS_Configuration -ConfigurationName Pester2).InitialDelay | Should be 99
+      Remove-LSS_Configuration -ConfigurationName Pester2
+    }
+
+    BeforeEach {
+      Function global:Write-Host() {}
+      Set-LSS_Configuration -ConfigurationName Pester -Parameter InitialDelay -Value 99
+    }
+
+    AfterEach {
+      Remove-LSS_Configuration -ConfigurationName Pester
+      Remove-Item -Path Function:\Write-Host
+    }
+  }
+
+  Context "'REMOVE'" {
+
+    It "Remove Configuration" {
+      Remove-LSS_Configuration -ConfigurationName Pester
+      (Get-LSS_Configuration -ConfigurationName Pester).InitialDelay | Should be $null
+    }
+
+    BeforeEach {
+      Function global:Write-Host() {}
+      Set-LSS_Configuration -ConfigurationName Pester -Parameter InitialDelay -Value 99
+    }
+
+    AfterEach {
+      Remove-Item -Path Function:\Write-Host
+    }
+  }
+}
+
+
