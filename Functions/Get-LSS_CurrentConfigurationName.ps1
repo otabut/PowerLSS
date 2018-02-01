2a405ba193e3486c96ab5f1aaedbbd519cf36da3
otabut <33980031+otabut@users.noreply.github.com>
2018-02-01 08:49:26 +0100

    Update Get-LSS_CurrentConfigurationName.ps1

--- Functions/Get-LSS_CurrentConfigurationName.ps1
+++ Functions/Get-LSS_CurrentConfigurationName.ps1
@@ -1,10 +1,14 @@
 Function Get-LSS_CurrentConfigurationName
 {
+  Param (
+    [parameter(Mandatory=$false)][Switch]$Quiet
+  )
+  
   $RegBasePath = "HKLM:\SOFTWARE\PowerLSS"
   $CurrentConfiguration = (Get-ItemProperty -Path $RegBasePath)."CurrentConfiguration"
   if (!($CurrentConfiguration))
   {
-    Write-Host "No current configuration has been set" -ForegroundColor Red
+    if (!($Quiet.IsPresent)) { Write-Host "No current configuration has been set" -ForegroundColor Red }
   }
   else
   {
