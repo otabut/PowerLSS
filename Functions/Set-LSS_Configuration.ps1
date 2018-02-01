3b9f373554dc0685f7fe5e37fa1d33cb37cc9774
otabut <33980031+otabut@users.noreply.github.com>
2018-02-01 09:14:28 +0100

    Update Set-LSS_Configuration.ps1

--- Functions/Set-LSS_Configuration.ps1
+++ Functions/Set-LSS_Configuration.ps1
@@ -3,7 +3,8 @@ Function Set-LSS_Configuration
   Param (
     [parameter(Mandatory=$true)][String]$ConfigurationName,
     [parameter(Mandatory=$true)][ValidateSet("LogFile","InitialDelay","Include","Exclude","ValidExitCodes","Retry","AllowReboot","ContinueIfRebootRequest","ContinueOnFailure","DisableAtTheEnd","ConsoleOutput","Output","CustomLogging","DontRunPreActions","DontRunPostActions","ScriptsFolder","ScheduledTaskLogFile")][String]$Parameter,
-    [parameter(Mandatory=$true)][String]$Value
+    [parameter(Mandatory=$true)][String]$Value,
+    [parameter(Mandatory=$false)][Switch]$Quiet
   )
   
   $ErrorActionPreference = "stop"
@@ -30,11 +31,18 @@ Function Set-LSS_Configuration
     if ((((Get-ChildItem -Path $RegBasePath).PSChildName).count -eq 0) -or ((Get-ItemProperty -Path $RegBasePath)."CurrentConfiguration" -eq $null))
     {
       New-ItemProperty -Path $RegBasePath -Name "CurrentConfiguration" -Value $ConfigurationName -PropertyType String -Force | Out-Null
-      Write-Host "Since there is no current configuration, $ConfigurationName has been set as default configuration" -ForegroundColor Yellow
+      if (!($Quiet.IsPresent)) { Write-Host "Since there is no current configuration, $ConfigurationName has been set as default configuration" -ForegroundColor Yellow }
     }
 
     New-ItemProperty -Path $RegPath -Name $Parameter -Value $Value -PropertyType string -Force | Out-Null
-    Write-Host "Parameter set successfully" -ForegroundColor Green
+    if ($Quiet.IsPresent)
+    {
+      Return [PSCustomObject]@{ConfigurationName=$ConfigurationName;Action="Set";Result="Success"}
+    }
+    else
+    {
+      Write-Host "Parameter set successfully" -ForegroundColor Green
+    }
   }
   catch
   {
