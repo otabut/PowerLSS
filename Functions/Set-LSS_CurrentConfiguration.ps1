2b5deb469017f8ba3d66fbb411e4c9cd95d0ba22
otabut <33980031+otabut@users.noreply.github.com>
2018-02-01 09:14:51 +0100

    Update Set-LSS_CurrentConfiguration.ps1

--- Functions/Set-LSS_CurrentConfiguration.ps1
+++ Functions/Set-LSS_CurrentConfiguration.ps1
@@ -1,7 +1,8 @@
 Function Set-LSS_CurrentConfiguration
 {
   Param (
-    [parameter(Mandatory=$false)][String]$ConfigurationName
+    [parameter(Mandatory=$false)][String]$ConfigurationName,
+    [parameter(Mandatory=$false)][Switch]$Quiet
   )
   
   $ErrorActionPreference = "stop"
@@ -18,11 +19,25 @@ Function Set-LSS_CurrentConfiguration
     if (Test-Path "$RegBasePath\$ConfigurationName" -PathType Container)
     {
       New-ItemProperty -Path $RegBasePath -Name "CurrentConfiguration" -Value $ConfigurationName -PropertyType String -Force | Out-Null
-      Write-Host "Current configuration updated successfully" -ForegroundColor Green
+      if ($Quiet.IsPresent)
+      {
+        Return [PSCustomObject]@{ConfigurationName=$ConfigurationName;Action="SetDefault";Result="Success"}
+      }
+      else
+      {
+        Write-Host "Current configuration updated successfully" -ForegroundColor Green
+      }
     }
     else
     {
-      Write-Host "Configuration $ConfigurationName cannot be found" -ForegroundColor Yellow
+      if ($Quiet.IsPresent)
+      {
+        Return [PSCustomObject]@{ConfigurationName=$ConfigurationName;Action="SetDefault";Result="Not found"}
+      }
+      else
+      {
+        Write-Host "Configuration $ConfigurationName cannot be found" -ForegroundColor Yellow
+      }
     }
   }
   catch
